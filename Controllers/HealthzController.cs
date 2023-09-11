f4fcf9719be2fa17218bd3bf0aaf2c8fda885462
Akinari Tsugo <akinaritsugo@microsoft.com>
2023-09-11 19:42:03 +0900

    healthチェックエンドポイントの追加

---
+++ Controllers/HealthzController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class HealthzController : Controller
+    {
+        public IActionResult Index()
+        {
+            return Json(new { Status = "OK" });
+        }
+    }
+}
